0578bee68e3eac00ab11afefc8aba630b2a6cf28
Felipe S. S. Schneider <schneider.felipe.5@gmail.com>
2023-09-04 16:37:53 -0300

    refactor(src/main.rs): separate message retrieval

--- src/main.rs
+++ src/main.rs
@@ -10,12 +10,7 @@ fn create_request(
     input: impl Into<String>,
 ) -> Result<async_openai::types::CreateChatCompletionRequest, async_openai::error::OpenAIError> {
     let temperature = 0.0;
-    let messages = [
-        async_openai::types::ChatCompletionRequestMessageArgs::default()
-            .role(async_openai::types::Role::User)
-            .content(input)
-            .build()?,
-    ];
+    let messages = retrieve_messages(input)?;
     let model = "gpt-3.5-turbo";
 
     async_openai::types::CreateChatCompletionRequestArgs::default()
@@ -23,4 +18,21 @@ fn create_request(
         .messages(messages)
         .model(model)
         .build()
+}
+
+#[inline]
+fn retrieve_messages(
+    input: impl Into<String>,
+) -> Result<
+    impl Into<Vec<async_openai::types::ChatCompletionRequestMessage>>,
+    async_openai::error::OpenAIError,
+> {
+    let messages = [
+        async_openai::types::ChatCompletionRequestMessageArgs::default()
+            .role(async_openai::types::Role::User)
+            .content(input)
+            .build()?,
+    ];
+
+    Ok(messages)
 }
