88759c7927d5680208e35c94091b527fbafc5e3f
Felipe S. S. Schneider <schneider.felipe.5@gmail.com>
2023-09-04 19:53:21 -0300

    refactor(src/main.rs): trim input

--- src/main.rs
+++ src/main.rs
@@ -110,7 +110,8 @@ fn create_request(
 }
 
 fn main() -> anyhow::Result<()> {
-    let input = read_input()?;
+    let raw_input = read_input()?;
+    let input = raw_input.trim();
     let message = create_user_message(input)?;
     let request = create_request(message)?;
     println!("{request:#?}");
