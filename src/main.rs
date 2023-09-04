82802441f1d0b58a7f5ec5a283f9bc88abb9ebfc
Felipe S. S. Schneider <schneider.felipe.5@gmail.com>
2023-09-04 17:30:54 -0300

    feat(src/main.rs): choose the cheapest model fitting the chat context

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,40 @@
-fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let input = std::io::read_to_string(std::io::stdin())?;
-    let request = create_request(input)?;
-    println!("{request:#?}");
-    Ok(())
+/// Available `OpenAI` models sorted by their prices.
+const MODELS: [&str; 4] = [
+    "gpt-3.5-turbo",     // $0.0015 / 1K tokens
+    "gpt-3.5-turbo-16k", // $0.003  / 1K tokens
+    "gpt-4",             // $0.03   / 1K tokens
+    "gpt-4-32k",         // $0.06   / 1K tokens
+];
+
+/// Get the cheapest model fitting the given chat context.
+#[inline]
+fn choose_model(
+    messages: &[async_openai::types::ChatCompletionRequestMessage],
+) -> anyhow::Result<Option<impl Into<String>>> {
+    let model = MODELS.into_iter().find(|model| {
+        tiktoken_rs::async_openai::get_chat_completion_max_tokens(model, messages)
+            .expect("model retrieval should not fail")
+            > 0
+    });
+
+    Ok(model)
 }
 
 #[inline]
 fn create_request(
     input: impl Into<String>,
-) -> Result<async_openai::types::CreateChatCompletionRequest, async_openai::error::OpenAIError> {
+) -> anyhow::Result<async_openai::types::CreateChatCompletionRequest> {
     let temperature = 0.0;
     let messages = retrieve_messages(input)?.into();
-    let model = choose_model(&messages);
+    let model = choose_model(&messages)?.expect("a model should be available");
 
-    async_openai::types::CreateChatCompletionRequestArgs::default()
+    let request = async_openai::types::CreateChatCompletionRequestArgs::default()
         .temperature(temperature)
         .messages(messages)
         .model(model)
-        .build()
+        .build()?;
+
+    Ok(request)
 }
 
 #[inline]
@@ -37,9 +54,9 @@ fn retrieve_messages(
     Ok(messages)
 }
 
-#[inline]
-fn choose_model(
-    _messages: &[async_openai::types::ChatCompletionRequestMessage],
-) -> impl Into<String> {
-    "gpt-3.5-turbo"
+fn main() -> anyhow::Result<()> {
+    let input = std::io::read_to_string(std::io::stdin())?;
+    let request = create_request(input)?;
+    println!("{request:#?}");
+    Ok(())
 }
