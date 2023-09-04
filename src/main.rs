a7f77c1942a0b0969c12927ad331b7d5da26c9b1
Felipe S. S. Schneider <schneider.felipe.5@gmail.com>
2023-09-04 16:16:45 -0300

    feat(src/main.rs): create simple request

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,26 @@
-fn main() -> std::io::Result<()> {
+fn main() -> Result<(), Box<dyn std::error::Error>> {
     let input = std::io::read_to_string(std::io::stdin())?;
-    println!("{input}");
+    let request = create_request(input)?;
+    println!("{request:#?}");
     Ok(())
+}
+
+#[inline]
+fn create_request(
+    input: String,
+) -> Result<async_openai::types::CreateChatCompletionRequest, async_openai::error::OpenAIError> {
+    let model = "gpt-3.5-turbo";
+    let max_tokens = 512u16;
+    let messages = [
+        async_openai::types::ChatCompletionRequestMessageArgs::default()
+            .content(input)
+            .role(async_openai::types::Role::User)
+            .build()?,
+    ];
+
+    async_openai::types::CreateChatCompletionRequestArgs::default()
+        .model(model)
+        .max_tokens(max_tokens)
+        .messages(messages)
+        .build()
 }
