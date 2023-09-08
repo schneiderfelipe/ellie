d72a86f853000758d0b513feededaf36fe52a289
Felipe S. S. Schneider <schneider.felipe.5@gmail.com>
2023-09-08 09:45:31 -0300

    refactor(src/main.rs): separate function engine

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
+mod functions;
+
 use async_openai::types as aot;
 use color_eyre::eyre;
 use eyre::{Context as _, ContextCompat as _};
+use functions::{try_compact_json, Functions};
 
 /// Temperature used in all requests.
 const TEMPERATURE: f32 = 0.0;
@@ -44,84 +47,6 @@ fn choose_model(messages: &[aot::ChatCompletionRequestMessage]) -> Option<&'stat
         messages_fit_model(model, messages)
             .expect("model retrieval of known models should never fail")
     })
-}
-
-/// Trim text
-/// and try to produce a compact JSON string out of it,
-/// returning an owned trimmed string if serialization fails.
-#[inline]
-fn try_compact_json(maybe_json: &str) -> String {
-    let maybe_json = maybe_json.trim();
-    serde_json::from_str::<serde_json::Value>(maybe_json)
-        .and_then(|value| serde_json::to_string(&value))
-        .unwrap_or_else(|_| maybe_json.into())
-}
-
-#[derive(Debug, serde::Deserialize)]
-struct Functions {
-    function: Vec<aot::ChatCompletionFunctions>,
-    provider: Vec<Provider>,
-}
-#[derive(Debug, serde::Deserialize)]
-struct Provider {
-    name: String,
-    command: String,
-    args: Vec<String>,
-}
-
-impl From<Functions> for Vec<aot::ChatCompletionFunctions> {
-    #[inline]
-    fn from(val: Functions) -> Self {
-        val.function
-    }
-}
-
-impl Functions {
-    #[inline]
-    fn load() -> eyre::Result<Self> {
-        let functions = std::fs::read_to_string("functions.toml")?;
-        let functions = toml::from_str(&functions)?;
-
-        // TODO: actual function specifications will be optionally retrieved directly
-        // from binaries/scripts in the future.
-        // There will be a way of overriding
-        // what the binary/script says using the configuration file,
-        // so that the
-        // configuration file is mostly clean most of the time.
-        // TODO: make sure there is a provider for each function,
-        // i.e.,
-        // provider.len() >= function.len()
-
-        Ok(functions)
-    }
-
-    #[inline]
-    fn is_empty(&self) -> bool {
-        self.provider.is_empty()
-    }
-
-    #[inline]
-    fn prune(self, _messages: &[aot::ChatCompletionRequestMessage]) -> eyre::Result<Self> {
-        // TODO: actually choose relevant functions based on the chat messages.
-        Ok(self)
-    }
-
-    #[inline]
-    fn call(&self, name: &str, arguments: &str) -> eyre::Result<String> {
-        let provider = self
-            .provider
-            .iter()
-            .find(|provider| provider.name == name)
-            .context("getting function provider")?;
-
-        // TODO: see <https://github.com/64bit/async-openai/blob/37769355eae63d72b5d6498baa6c8cdcce910d71/examples/function-call-stream/src/main.rs#L67>
-        // and <https://github.com/64bit/async-openai/blob/37769355eae63d72b5d6498baa6c8cdcce910d71/examples/function-call-stream/src/main.rs#L84>.
-        let content = duct::cmd(&provider.command, &provider.args)
-            .stdin_bytes(arguments)
-            .read()?;
-
-        Ok(try_compact_json(&content))
-    }
 }
 
 /// Call the given function with the given arguments
