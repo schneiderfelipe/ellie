9461715dcb6c0a9620b8469e0e1261594234a080
Felipe S. S. Schneider <schneider.felipe.5@gmail.com>
2023-09-08 14:53:35 -0300

    refactor(src/main.rs): now a single file

--- get_current_weather.py
+++ get_current_weather.py
@@ -1,8 +1,38 @@
 from sys import argv
-from subprocess import call
+import json
 
 match argv[1:]:  # ignore "get_current_weather.py"
-    case ["specification"]:
-        call(["cat", "current_weather_specification.json"])
     case []:
-        call(["cat", "current_weather.json"])
+        print(
+            json.dumps(
+                {
+                    "location": "Boston, MA",
+                    "temperature": "72",
+                    "unit": None,
+                    "forecast": ["sunny", "windy"],
+                }
+            )
+        )
+    case ["specification"]:
+        print(
+            json.dumps(
+                {
+                    "name": "get_current_weather",
+                    "description": "Get the current weather in a given location",
+                    "parameters": {
+                        "type": "object",
+                        "required": ["location"],
+                        "properties": {
+                            "location": {
+                                "type": "string",
+                                "description": "The city and state, e.g. San Francisco, CA",  # noqa: E501
+                            },
+                            "unit": {
+                                "type": "string",
+                                "enum": ["celsius", "fahrenheit"],
+                            },
+                        },
+                    },
+                }
+            )
+        )
