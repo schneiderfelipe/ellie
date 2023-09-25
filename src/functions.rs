3e748c798fe02b90bfcf6e82d8852d8f2aefc2e5
Felipe S. S. Schneider <schneider.felipe.5@gmail.com>
2023-09-25 14:03:42 -0300

    refactor(src/functions.rs): merge function call into confirm dialog

--- src/functions.rs
+++ src/functions.rs
@@ -61,10 +61,12 @@ impl Provider {
     fn call(&self, arguments: &str) -> color_eyre::eyre::Result<String> {
         use color_eyre::eyre::Context as _;
 
-        log::info!("{name}({arguments})", name = self.name);
         let content = if self.safe
             || dialoguer::Confirm::with_theme(&dialoguer::theme::ColorfulTheme::default())
-                .with_prompt("Execute function provider?")
+                .with_prompt(format!(
+                    "{name}({arguments})\n\nWould you like to execute this function provider?",
+                    name = self.name
+                ))
                 .interact()?
         {
             duct::cmd(&self.command, &self.args)
